9c945413f6f5f3005d115e8ce94c6b81680aaebb
enegorikun7777 <ktq2390323@stu.o-hara.ac.jp>
2024-05-01 11:23:40 +0900

    削除

---
+++ src/scoremanager/main/SubjectDeleteAction.java
@@ -0,0 +1,60 @@
+package scoremanager.main;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.School;
+//科目情報が必要なためsubjectのbeanが必要
+import bean.Subject;
+//セッションでユーザー情報を取得する必要があるためteacherのbeanが必要
+import bean.Teacher;
+import dao.SubjectDAO;
+import tool.Action;
+
+public class SubjectDeleteAction extends Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		// セッションでユーザー情報を取得する
+		// ユーザーの状態を保持するために必要だからこのコードが必要
+		HttpSession session = request.getSession();
+		Teacher teacher = (Teacher) session.getAttribute("user");
+
+		// セッションから取得したユーザー情報から学校情報を取得
+		School school = teacher.getSchool();
+
+		// 科目の一覧からその科目の削除を選択するから他の情報はいらない。
+		String cd = request.getParameter("subject_cd");
+
+		System.out.println("delete action 結果");
+		System.out.println(cd);
+
+		// リクエストスコープが何で必要なんか?
+		// リクエストスコープはそもそもその処理が終わるまで一時的に情報を保持しておくためのもの
+		// ここの処理の場合だと、削除して大丈夫？っていうsubject_delete.jspに画面が移動して
+		// 確定ボタンが押されるまでの間、削除するかどうかの判断ができないから保持しておく。
+		SubjectDAO subjectDAO = new SubjectDAO();
+		Subject subject = subjectDAO.get(cd, school);
+
+		System.out.println("subjectdaoからget(cd, school)ができているか");
+		System.out.println(subject);
+
+		request.setAttribute("subject", subject);
+
+		if (subject != null) {
+			// リクエストスコープにセットする処理
+			System.out.println("Subjectオブジェクトはnullではありません。");
+		} else {
+			// null の場合の処理
+			System.out.println("Subjectオブジェクトはnullです。");
+		}
+
+		// 全部の処理が完了したら科目を削除するよ大丈夫かい？ってページにフォワードする
+		// この上までの処理は科目一覧のところで削除ボタンを押されたことによって行われる処理
+		// ここから下が処理が終わって削除を実行するかどうかの処理になる
+		request.getRequestDispatcher("subject_delete.jsp").forward(request, response);
+
+	}
+}
---
+++ src/scoremanager/main/SubjectDeleteExecuteAction.java
@@ -0,0 +1,36 @@
+package scoremanager.main;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.Subject;
+import dao.SubjectDAO;
+import tool.Action;
+
+public class SubjectDeleteExecuteAction extends Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+//		SubjectDeleteActionのsetAttribute()のところでリクエストスコープに科目コードを格納している
+//		その科目コードを取得してここで科目を削除する処理を行う
+//		まずは科目コードを取得
+		String cd = request.getParameter("subject_cd");
+
+		System.out.println("executeでrequest.getParameterが正常に動いているか");
+		System.out.println(cd);
+
+//		ここで科目情報を削除する機能を実行させる（executeが実行という意味）
+		SubjectDAO subjectDao = new SubjectDAO();
+		Subject subject = new Subject();
+
+		System.out.println("4 null");
+		System.out.println(cd);
+
+		subject.setCd(cd);
+		subjectDao.delete(subject);
+
+//		削除を実行したのち削除が完了しましたよーのページにリダイレクト
+		response.sendRedirect("subject_delete_done.jsp");
+	}
+}
